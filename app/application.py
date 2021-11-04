aae82f41add7167ae8ab290cf82e05dcfe58bef0
mohammedAljadd <mohammedaljadd8@gmail.com>
2021-11-04 15:13:25 +0100

    add form

--- app/application.py
+++ app/application.py
@@ -1,15 +1,25 @@
 from app import app, mysql
-
+from flask import render_template, request, redirect
+from app.utils import *
 
 
  
 
 
-@app.route("/")
-def index():
-    cursor = mysql.connection.cursor()
-    cursor.execute(''' TRUNCATE user; ''')
-    cursor.execute(''' INSERT INTO `user` (`id`, `name`, `password`) VALUES ('1', 'my_name', 'my_password'); ''')
-    mysql.connection.commit()
-    cursor.close()
-    return "S"
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/user-db", methods=["GET", "POST"])
+def user_db():
+
+    if request.method == "POST":
+        
+        req = request.form
+        email = req.get("email")
+        password = request.form["password"]
+        db(mysql, email, password)
+        
+
+    return redirect("/home")
---
+++ app/utils.py
@@ -0,0 +1,13 @@
+def db(mysql, email, password):
+
+    cursor = mysql.connection.cursor()
+    #cursor.execute(''' TRUNCATE user; ''')
+    
+
+    cursor.execute("SELECT COUNT(email) FROM `user`")
+    result=cursor.fetchone()
+    next_id = result[0] + 1
+    
+    cursor.execute(" INSERT INTO `user` VALUES (%s, %s, %s)", (next_id, email, password))
+    mysql.connection.commit()
+    cursor.close()
